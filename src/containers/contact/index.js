214d555fff3a874d6058a65073527f82a05b61ab
Dieguin <diego.segura@geniac.com>
2016-10-12 20:46:05 +0200

    :tada: [contact] add mailgun endpoint and logic

--- src/containers/contact/index.js
+++ src/containers/contact/index.js
@@ -1,7 +1,7 @@
 import template from './contact.html';
 import './contact.styl';
 
-function contactComponent($http) {
+function contactComponent($http, $log) {
 
   'ngInject';
 
@@ -10,12 +10,27 @@ function contactComponent($http) {
   this.contactForm.message = '';
 
   this.sendEmail = () => {
-    const data = this.contactForm;
-    const emailEndpoint = '';
-    $http.post(emailEndpoint, data).then(() => {
-      // TODO
-    }).catch(() => {
-      // TODO
+    const domain = 'sandbox1b7008a155644c6c814eba05dc5c59e1.mailgun.org';
+    const data = {
+      from: this.contactForm.email,
+      to: 'diego.segura@mimetix.com',
+      subject: 'Contacto Web Mimetix',
+      text: this.contactForm.message,
+    };
+    const mailgunApiKey = 'api:key-deb367be4c2450889fd03e2bed223027';
+    const config = {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization: `Basic ${mailgunApiKey}`,
+      },
+      method: 'POST',
+    };
+    const emailEndpoint = `https://api.mailgun.net/v3/${domain}/messages`;
+
+    $http.post(emailEndpoint, data, config).then((response) => {
+      $log.log(response);
+    }).catch((err) => {
+      $log.log(err);
     });
   };
 }
